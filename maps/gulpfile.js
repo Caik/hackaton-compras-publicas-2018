eac9acf7f5fe1fbda4a765a53c9ed964d73e43fa
BruAndreo <brunolino2026@gmail.com>
2018-05-06 02:02:09 -0300

    Apresentando localização no mapa

---
+++ maps/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'), 
+    connect = require('gulp-connect-multi')();
+  
+gulp.task('arquivos', function(){
+    gulp.src(['./src/**/**/*.*'])
+        .pipe(gulp.dest('./build'))
+        .pipe(connect.reload())
+});
+  
+gulp.task('servidor', connect.server({
+ 	root: ['build'],
+ 	port: 1337,
+ 	livereload: true
+}));
+
+gulp.task('watch', function(){
+    gulp.watch(['src/**/**'], ['arquivos']);
+});
+
+gulp.task('default', ['arquivos', 'servidor', 'watch']);
